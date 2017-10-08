1afbbbb807308d5241838aa98600108582c21c05
Peter Maydell <pmaydell@chiark.greenend.org.uk>
2017-10-08 16:59:10 +0100

    Implement parsing of an individual user page
    Implement code to parse a single user page and get its statistics.
    
    Signed-off-by: Peter Maydell <pmaydell@chiark.greenend.org.uk>

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ extern crate select;
 use select::document::Document;
 use select::predicate::{Predicate, Class, Name};
 
+use std::collections::HashMap;
+
 fn parse_mainpage(document: Document) -> Vec<String> {
     // Parse the top level rankings page, the relevant part of which looks like
     //	<table class="table">
@@ -48,6 +50,56 @@ fn parse_mainpage(document: Document) -> Vec<String> {
     return users;
 }
 
+#[derive(Debug)]
+struct UserInfo {
+    name: String,
+    countmap: HashMap<String, f64>,
+    totalpoints: f64,
+}
+
+fn parse_userpage(document: Document) -> UserInfo {
+    // We want to grab:
+    //  * the raw page counts for each category from the content tab
+    //  * the total point value
+    //  * the daily series data from the javascript
+    // TODO: maybe we should get the main page for each language instead?
+    let username = document.find(Class("avatar")).next().unwrap().attr("alt").unwrap();
+
+    let tablehead = document.find(Class("table-bordered").descendant(Name("thead"))).next().unwrap();
+    let tablebody = document.find(Class("table-bordered").descendant(Name("tbody"))).next().unwrap();
+    // Pull the category names out of the table head. We discard the first <th> (empty)
+    // and the last ("Total")
+    let headings = tablehead.find(Name("th"))
+        .map(|tag| tag.text())
+        .skip(1)
+        .filter(|x| x != "Total")
+        .collect::<Vec<_>>();
+    // First <tr> in here has the raw-page counts
+    let rawcounts = tablebody.find(Name("tr")).next().unwrap()
+        .find(Name("td"))
+        .map(|tag| tag.text())
+        .skip(1)
+        .filter(|x| x != "")
+        .map(|x| x.parse::<f64>().unwrap())
+        .collect::<Vec<_>>();
+    // Create a category -> count hashtable
+    let countmap: HashMap<String, f64> =
+        headings.iter().cloned().zip(rawcounts).collect();
+
+    // Now get the total point value out of the 2nd <tr>
+    let totalpoints = tablebody.find(Name("tr")).nth(1).unwrap()
+        .find(Name("td"))
+        .last().unwrap()
+        .text()
+        .parse().unwrap();
+
+    UserInfo {
+        name : String::from(username),
+        countmap: countmap,
+        totalpoints : totalpoints,
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -58,6 +110,7 @@ mod tests {
     use select::document::Document;
     // TODO can we just import everything from the root here?
     use parse_mainpage;
+    use parse_userpage;
 
     #[test]
     fn test_parse_mainpage() {
@@ -66,5 +119,17 @@ mod tests {
         // Check that we parsed our sample document plausibly
         assert_eq!(users.len(), 28);
         assert_eq!(users[0], "801");
+    }
+
+    #[test]
+    fn test_parse_userpage() {
+        let document = Document::from(include_str!("userpage.html"));
+        let user = parse_userpage(document);
+        println!{"{:#?}", user};
+        assert_eq!(user.name, "shenmedemo");
+        assert_eq!(user.totalpoints, 638.9);
+        assert_eq!(user.countmap.len(), 10);
+        let bookcount = user.countmap.get("Book").unwrap();
+        assert!(bookcount == &91.0);
     }
 }
